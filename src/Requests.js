3d083bc3ebc22d1657163894d18acc8bdf28bca9
joacomesa22 <joacomesa@gmail.com>
2023-08-27 23:48:17 -0300

    first commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "19117ba1f4fb4d493168d419a51b990d";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestNowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import requests from "../Requests";
+
+const Hero = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(requests.requestNowPlaying)
+      .then((res) => setMovies(res.data.results));
+  }, []);
+
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
+  return (
+    <div className="w-full h-[80vh] relative">
+      <div className="absolute w-full h-full bg-gradient-to-r from-[var(--black)]"></div>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+        alt={movie?.title}
+        className="w-full h-full object-cover"
+      />
+      <div className="absolute top-1/2 transform -translate-y-1/2 max-w-[660px] flex flex-col gap-4 px-10">
+        <p className="text-sm text-[var(--grey)]">
+          Released {movie?.release_date}
+        </p>
+        <h1 className="text-6xl font-bold">{movie?.title}</h1>
+        <p>{truncateString(movie?.overview, 120)}</p>
+        <div className="flex gap-4">
+          <button className="px-6 py-2 bg-[var(--fucsia)] hover:bg-[var(--fucsiaDarker)] hover:text-[--white] duration-300">
+            Play
+          </button>
+          <button className="px-4 py-2 bg-white/5 backdrop-blur-sm border border-solid border-[var(--white)] hover:bg-[var(--white)] hover:text-[var(--black)] duration-300">
+            Watch Later
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
